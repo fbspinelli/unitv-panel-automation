d4eb3384f220aa0e26d150b342d5170dddc94147
quarkusio <no-reply@quarkus.io>
2025-04-29 00:43:46 +0000

    Initial commit

---
+++ src/test/java/com/nando/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.nando;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
